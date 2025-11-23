b202087972ca683fa19877c98f04c7f76be4ea53
Pratik Khaire <Pratik.shekhar.khaire@gmail.com>
2025-11-23 15:25:38 +0530

    Add weather fetching functionality with Zod validation and UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { getWeather } from "./api";
+import Card from "./components/Card";
 
 const App = () => {
-  return (
-    <div className=' text-3xl bg-red-500'>App</div>
-  )
-}
+  const { data } = useQuery({
+    queryKey: ["weather"],
+    queryFn: () => getWeather({ lat: 50, lon: 50 }),
+  });
 
-export default App  
+  return (
+    <>
+      <Card>{JSON.stringify(data?.current)}</Card>
+      <Card>{JSON.stringify(data)}</Card>
+      {/* <Card>{JSON.stringify(data)?.daily}</Card> */}
+      {JSON.stringify(data)}
+    </>
+  );
+};
+
+export default App;
--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,11 @@
+import { WeatherSchema } from "./schemas/weatherSchema";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 export async function getWeather({ lat, lon }: { lat: number; lon: number }) {
   const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={API key}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,alerts&appid=${API_KEY}`
   );
   const data = await res.json();
-  return data;
+  return WeatherSchema.parse(data);
 }
